9eb62d004c5db23a543c84066f0b3d961cce61f4
Replit user <>
2023-08-17 20:30:31 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -109,9 +109,6 @@ void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
     }
 }
 
-  
-}
-
 /*
 Ejercicio 5: Comprobación de Ordenación
 Descripción: Escribe una función que tome un arreglo y su tamaño,
