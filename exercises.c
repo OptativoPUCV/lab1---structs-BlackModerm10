60dd6bb89063f1110541bc99f742bdffbeaaf0f6
Replit user <>
2023-08-18 22:11:50 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -191,24 +191,18 @@ Nodo *crearListaEnlazada(int arr[], int size) {
     for (int i = 0; i < size; i++) {
         Nodo *nuevoNodo = (Nodo *)malloc(sizeof(Nodo)); // Crear un nuevo nodo
 
-        if (nuevoNodo == NULL) {
-            // Manejo de error si la asignación de memoria falla
-            return NULL;
-        }
+
 
         nuevoNodo->numero = arr[i];
         nuevoNodo->siguiente = NULL;
 
         if (actual == NULL) {
-            // Si es el primer nodo, actual y primero apuntan al nuevo nodo
             actual = nuevoNodo;
             primero = nuevoNodo;
         } else {
-            // Enlazar el nodo actual al nuevo nodo
             actual->siguiente = nuevoNodo;
-            actual = nuevoNodo; // Mover actual al nuevo nodo
+            actual = nuevoNodo; 
         }
     }
-
-    return primero; // Devolver un puntero al primer nodo de la lista
+    return primero; 
 }
